6a3f64a56f2cfd2994120559c9182816e3a38b52
Idan Plonsky <plonsky@gmail.com>
2020-04-11 13:52:35 +0300

    first commit

---
+++ Math2ndGrade/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Math2ndGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SubtractionUntil20 subtractionUntil20 = new SubtractionUntil20();
+            subtractionUntil20.GenerateExercises(60);
+
+//            MultiplactionUntil100 multiplactionUntil100 = new MultiplactionUntil100();
+//            multiplactionUntil100.GenerateExercises(120);
+        }
+    }
+}
---
+++ Math2ndGrade/SubtractionUntil20.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math2ndGrade
+{
+    class SubtractionUntil20
+    {
+        internal void GenerateExercises(int excersicesNum)
+        {
+            GenerateExcercises(10, 5, 5, 4, excersicesNum);
+        }
+
+        private void GenerateExcercises(int subtrahendFirstNun, int subtrahendRange, int minuendFirstNum, int minuendRange, int excercisesNum)
+        {
+            for (int i = 0; i < excercisesNum/3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    int subtrahend = new Random().Next(subtrahendFirstNun, subtrahendFirstNun+subtrahendRange+1);
+                    int minuend = new Random().Next(minuendFirstNum, minuendFirstNum + minuendRange + 1);
+                    string exercise = $"{subtrahend}-{minuend} =";
+                    Console.Write($"{exercise}                                ");
+                    if (minuend != 10)
+                    {
+                        Console.Write($" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
